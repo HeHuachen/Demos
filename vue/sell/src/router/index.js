a0c1af1fd6cdef409afedfbedfbf25ee970b151d
HeHuachen <2427963705@qq.com>
2018-02-28 00:23:55 +0800

    vue router simple

--- vue/sell/src/router/index.js
+++ vue/sell/src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import goods from '@/components/goods/goods'
 import ratings from '@/components/ratings/ratings'
 import seller from '@/components/seller/seller'
@@ -7,11 +8,13 @@ import seller from '@/components/seller/seller'
 Vue.use(Router)
 
 const routes = [
-  { path: '/goods', component: goods },
-  { path: '/ratings', component: ratings },
-  { path: '/seller', component: seller }
+  { path: '/goods', name: 'goods', component: goods },
+  { path: '/ratings', name: 'ratings', component: ratings },
+  { path: '/seller', name: 'seller', component: seller }
 ]
 
 export default new Router({
+  mode: 'history',
+  linkActiveClass: 'active',
   routes
 })
